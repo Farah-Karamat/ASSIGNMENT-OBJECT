4e721ab1db10f530040e334e2436f9ed51068909
Farah Karamat <farahkaramat@gmail.com>
2025-04-11 19:14:08 -0700

    ASSIGNMENT-OBJECT
    MULTIPLE DATA STORE IN AN OBJECT

---
+++ ASSIGNMENT-OBJECT/app.js
@@ -0,0 +1,205 @@
+//STORE OF MULTIPLE STUDENTS DATA IN AN OBJECT.
+
+var students = {
+sara: {
+    fullName: "Sara",
+    fatherName: "Muhammad Nisar",
+    gender: "Female",
+    email: "sara@gmail.com",
+    qualification: "Olevel",
+    contact: 03234567412,
+    address: "Faisal Colony Karachi"
+},
+
+hamna: {
+    fullName: "Hamna",
+    fatherName: "Muhammad Ahmad",
+    gender: "Female",
+    email: "hamna@gmail.com",
+    qualification: "Olevel",
+    contact: 03234363212,
+    address: "Faisal Base Karachi"
+},
+
+eman: {
+    fullName: "Eman",
+    fatherName: "Ashraf Hussain",
+    gender: "Female",
+    email: "eman@gmail.com",
+    qualification: "Olevel",
+    contact: 03514567891,
+    address: "Malir Cantt Karachi"
+},
+
+mina: {
+    fullName: "Meena",
+    fatherName: "Muhammad Anas",
+    gender: "Female",
+    email: "meena@gmail.com",
+    qualification: "Olevel",
+    contact: 03234566785,
+    address: "Malir Hault Karachi"
+},
+
+humaira: {
+    fullName: "Humaira",
+    fatherName: "Muhammad Ahsan",
+    gender: "Female",
+    email: "humaira@gmail.com",
+    qualification: "Olevel",
+    contact: 03234412367,
+    address: "Korangi15 Karachi"
+},
+
+Amna: {
+    fullName: "Sara",
+    fatherName: "Ehsan Ahmad",
+    gender: "Female",
+    email: "amna@gmail.com",
+    qualification: "Olevel",
+    contact: 03222468502,
+    address: "Federal B-Area Karachi"
+    },
+
+daisy: {
+    fullName: "Daisy",
+    fatherName: "Edward",
+    gender: "Female",
+    email: "daisy@gmail.com",
+    qualification: "Olevel",
+    contact: 03254123678,
+    address: "DHA Phase-6 Karachi"
+},
+
+asma: {
+    fullName: "Asma",
+    fatherName: "Muhammad Zakariya",
+    gender: "Female",
+    email: "asma@gmail.com",
+    qualification: "Olevel",
+    contact: 03251236987,
+    address: "DHA Phase-7 Karachi"
+},
+
+hina: {
+    fullName: "Hina",
+    fatherName: "Muhammad Razaq",
+    gender: "Female",
+    email: "hina@gmail.com",
+    qualification: "Olevel",
+    contact: 03226547236,
+    address: "DHA Phase-2 Karachi"
+},
+
+ahmed: {
+    fullName: "Ahmed",
+    fatherName: "Muhammad Pasha",
+    gender: "Male",
+    email: "ahmed@gmail.com",
+    qualification: "Olevel",
+    contact: 03232234561,
+    address: "Korangi-16 Karachi"
+},
+
+raza: {
+    fullName: "Raza",
+    fatherName: "Arif Khan",
+    gender: "Male",
+    email: "raza@gmail.com",
+    qualification: "Olevel",
+    contact: 03158976543,
+    address: "Malir-15 Karachi"
+},
+
+iqbal: {
+    fullName: "Iqbal",
+    fatherName: "Hamid Rehman",
+    gender: "Male",
+    email: "iqbal@gmail.com",
+    qualification: "Matric",
+    contact: 0325897456,
+    address: "Ayesha Manzil Karachi"
+},
+
+ali: {
+    fullName: "Ali",
+    fatherName: "Muhammad Yousaf",
+    gender: "Male",
+    email: "ali@gmail.com",
+    qualification: "Matric",
+    contact: 03325647891,
+    address: "Malir-15 Karachi"
+},
+
+asha: {
+    fullName: "Asha",
+    fatherName: "Ravi Mehta",
+    gender: "Female",
+    email: "asha@gmail.com",
+    qualification: "Matric",
+    contact: 03654789122,
+    address: "Federal-B Area Karachi"
+},
+
+daniyal: {
+    fullName: "Daniyal",
+    fatherName: "Muhammad Taha",
+    gender: "Male",
+    email: "daniyal@gmail.com",
+    qualification: "Olevel",
+    contact: 0324169825,
+    address: "Defence-5 Karachi"
+},
+
+walid: {
+    fullName: "Walid",
+    fatherName: "Arsalan Ahmed",
+    gender: "Male",
+    email: "walid@gmail.com",
+    qualification: "Olevel",
+    contact: 03246985231,
+    address: "Defence-1 Karachi"
+},
+
+fatima: {
+    fullName: "Fatima",
+    fatherName: "Muhammad Ali",
+    gender: "Female",
+    email: "fatima@gmail.com",
+    qualification: "Olevel",
+    contact: 03521365479,
+    address: "Faisal Colony Karachi"
+},
+
+Lubna: {
+    fullName: "Lubna",
+    fatherName: "Shahnawaz Ali",
+    gender: "Female",
+    email: "lubna@gmail.com",
+    qualification: "Olevel",
+    contact: 03521852470,
+    address: "Korangi near Next Cola Factory Karachi"
+},
+
+Tayab: {
+    fullName: "Tayab",
+    fatherName: "Ghous Raza",
+    gender: "Male",
+    email: "tayab@gmail.com",
+    qualification: "Matric",
+    contact: 03521332651,
+    address: "Faisal Colony Karachi"
+},
+
+Mehak: {
+    fullName: "Mehak",
+    fatherName: "Azam Khan",
+    gender: "Female",
+    email: "mehak@gmail.com",
+    qualification: "Olevel",
+    contact: 03335412587,
+    address: "SITE area Karachi"
+},
+
+}
+console.log(students)
